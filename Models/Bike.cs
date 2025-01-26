de810be0784bb83e1c1012fe79ce3463af2f5734
Nisse Bengtsson <benzzon@gmail.com>
2025-01-26 21:06:14 +0100

    Updated solution so bikes can be loaded from benzzon.se.

---
+++ Models/Bike.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BikeInfo.Models
+{
+    public class Bike
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("manufacturer")]
+        public string Manufacturer { get; set; }
+
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+
+        [JsonPropertyName("photo")]
+        public string Photo { get; set; }
+
+        [JsonPropertyName("submodel")]
+        public string Submodel { get; set; }
+
+        [JsonPropertyName("enginepowerhp")]
+        public string EnginePowerHp { get; set; }
+
+        [JsonPropertyName("accelerationto100")]
+        public object AccelerationTo100 { get; set; }  // Can be string or boolean
+
+        [JsonPropertyName("countryoforigin")]
+        public string CountryOfOrigin { get; set; }
+
+        [JsonPropertyName("cylindersalignment")]
+        public object CylindersAlignment { get; set; }
+
+        [JsonPropertyName("displacement")]
+        public string Displacement { get; set; }
+
+        [JsonPropertyName("dryweight")]
+        public string DryWeight { get; set; }
+
+        [JsonPropertyName("enginestroke")]
+        public string EngineStroke { get; set; }
+
+        [JsonPropertyName("fronttire")]
+        public string FrontTire { get; set; }
+
+        [JsonPropertyName("fuelcapacityliters")]
+        public string FuelCapacityLiters { get; set; }
+
+        [JsonPropertyName("fuelsystem")]
+        public string FuelSystem { get; set; }
+
+        [JsonPropertyName("oilcapacityliters")]
+        public string OilCapacityLiters { get; set; }
+
+        [JsonPropertyName("reartire")]
+        public string RearTire { get; set; }
+
+        [JsonPropertyName("topspeed")]
+        public string TopSpeed { get; set; }
+
+        [JsonPropertyName("valvespercylinder")]
+        public string ValvesPerCylinder { get; set; }
+
+        [JsonPropertyName("yearsformodelgen")]
+        public string YearsForModelGen { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using BikeApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.FluentUI.AspNetCore.Components;
+using NLog.Extensions.Logging;
 
 namespace BikeInfo
 {
@@ -9,11 +12,26 @@ namespace BikeInfo
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            // Configure NLog
+            var loggerFactory = LoggerFactory.Create(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+                loggingBuilder.AddNLog();
+            });
+
+            var logger = loggerFactory.CreateLogger("App");
+            logger.LogInformation("Application starting...");
+
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddFluentUIComponents();
+
+            builder.Services.AddScoped<BikeService>();
+            builder.Services.AddSingleton(loggerFactory);
 
             await builder.Build().RunAsync();
         }
